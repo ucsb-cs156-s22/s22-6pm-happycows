af3269177ff64cbf116b3cace4d1074d4e237687
Seth VanBrocklin <vanbrocklin@ucsb.edu>
2022-02-16 15:43:27 -0800

    solution to eslint warning and old code commented out above

--- frontend/src/main/components/Commons/ProfitsTable.js
+++ frontend/src/main/components/Commons/ProfitsTable.js
@@ -2,6 +2,7 @@ import React from "react";
 import OurTable from "main/components/OurTable";
 
 export default function ProfitsTable({ profits }) {
+    //Not sure why this code doesn't work 
     // const columns = [
     //     {
     //         Header: "Profit",
@@ -12,6 +13,7 @@ export default function ProfitsTable({ profits }) {
     //         accessor: "date",
     //     }
     // ];
+    // const memoizedColumns = React.useMemo(() => columns, [columns]);
 
     // Stryker disable ArrayDeclaration : [columns] and [students] are performance optimization; mutation preserves correctness
     const memoizedColumns = React.useMemo(() => 
