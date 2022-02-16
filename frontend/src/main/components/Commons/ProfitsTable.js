3bb21ae6ae26a995afff4afe6916c55ab5b95750
Seth VanBrocklin <vanbrocklin@ucsb.edu>
2022-02-16 15:14:28 -0800

    trying to fix eslint warning

--- frontend/src/main/components/Commons/ProfitsTable.js
+++ frontend/src/main/components/Commons/ProfitsTable.js
@@ -2,16 +2,16 @@ import React from "react";
 import OurTable from "main/components/OurTable";
 
 export default function ProfitsTable({ profits }) {
-    const columns = [
-        {
-            Header: "Profit",
-            accessor: "profit",
-        },
-        {
-            Header: "Date",
-            accessor: "date",
-        }
-    ];
+    // const columns = [
+    //     {
+    //         Header: "Profit",
+    //         accessor: "profit",
+    //     },
+    //     {
+    //         Header: "Date",
+    //         accessor: "date",
+    //     }
+    // ];
 
     // Stryker disable ArrayDeclaration : [columns] and [students] are performance optimization; mutation preserves correctness
     const memoizedColumns = React.useMemo(() => {const columns = [
