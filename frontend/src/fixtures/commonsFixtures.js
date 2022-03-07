ded2293e173bde73309230d584af7320b6c24a70
rsdharmaji <59861195+rsdharmaji@users.noreply.github.com>
2022-03-07 13:38:04 -0800

    rd - add milkPrice + startDate to commonsFixtures + ISO date format

--- frontend/src/fixtures/commonsFixtures.js
+++ frontend/src/fixtures/commonsFixtures.js
@@ -4,25 +4,32 @@ const commonsFixtures = {
             "id": 5,
             "name": "Seths Common",
             "day": 5,
-            "endDate": "6/11/2021",
+            "startDate": "20131219",
+            "endDate": "20141212",
             "totalPlayers": 50,
             "cowPrice": 15,
+            "milkPrice": 10,
         },
         {
             "id": 4,
             "name": "Kevin's Commons",
             "day": 5,
-            "endDate": "6/11/2021",
+            "startDate": "20181219",
+            "endDate": "20191212",
             "totalPlayers": 50,
             "cowPrice": 15,
+            "milkPrice": 10,
         },
         {
             "id": 1,
             "name": "Anika's Commons",
             "day": 5,
+            "startDate": "20151219",
+            "endDate": "20161212",
             "endDate": "6/11/2021",
             "totalPlayers": 50,
             "cowPrice": 15,
+            "milkPrice": 10,
         }
     ],
     oneCommons:
@@ -31,9 +38,11 @@ const commonsFixtures = {
                 "id": 1,
                 "name": "Anika's Commons",
                 "day": 5,
-                "endDate": "6/11/2021",
+                "startDate": "20171219",
+                "endDate": "20181212",
                 "totalPlayers": 50,
                 "cowPrice": 15,
+                "milkPrice": 10,
             }
         ]
 }
