330593dd4b4412ca93d72f3410863beab98f8291
rsdharmaji <59861195+rsdharmaji@users.noreply.github.com>
2022-03-08 19:39:50 -0800

    remove erroneously added file from PR
    This reverts commit 7f29e1119f8f78aefd22a8b409ebbd7560e13fee.

--- frontend/src/fixtures/commonsFixtures.js
+++ frontend/src/fixtures/commonsFixtures.js
@@ -4,32 +4,25 @@ const commonsFixtures = {
             "id": 5,
             "name": "Seths Common",
             "day": 5,
-            "startDate": "20131219",
-            "endDate": "20141212",
+            "endDate": "6/11/2021",
             "totalPlayers": 50,
             "cowPrice": 15,
-            "milkPrice": 10,
         },
         {
             "id": 4,
             "name": "Kevin's Commons",
             "day": 5,
-            "startDate": "20181219",
-            "endDate": "20191212",
+            "endDate": "6/11/2021",
             "totalPlayers": 50,
             "cowPrice": 15,
-            "milkPrice": 10,
         },
         {
             "id": 1,
             "name": "Anika's Commons",
             "day": 5,
-            "startDate": "20151219",
-            "endDate": "20161212",
             "endDate": "6/11/2021",
             "totalPlayers": 50,
             "cowPrice": 15,
-            "milkPrice": 10,
         }
     ],
     oneCommons:
@@ -38,11 +31,9 @@ const commonsFixtures = {
                 "id": 1,
                 "name": "Anika's Commons",
                 "day": 5,
-                "startDate": "20171219",
-                "endDate": "20181212",
+                "endDate": "6/11/2021",
                 "totalPlayers": 50,
                 "cowPrice": 15,
-                "milkPrice": 10,
             }
         ]
 }
