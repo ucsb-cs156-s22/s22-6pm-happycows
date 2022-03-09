f81d78bd512a0c57ad8fe34650ecd091ef7e2236
Erin Ambriz <erinambriz@ucsb.edu>
2022-03-08 18:45:34 -0800

    ea-working on controller tests, at about 25% cov

--- src/main/java/edu/ucsb/cs156/happiercows/entities/Profit.java
+++ src/main/java/edu/ucsb/cs156/happiercows/entities/Profit.java
@@ -13,7 +13,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.JoinColumn;
 
 @Data
