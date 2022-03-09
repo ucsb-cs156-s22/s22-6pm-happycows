b7a40c99269871d318978c50d91a99f8d7e4ae64
Ryan Mosalem <ryanmosalem@ucsb.edu>
2022-03-08 23:11:10 -0800

    rm - updating formatting for dates

--- frontend/src/main/components/Commons/Profits.js
+++ frontend/src/main/components/Commons/Profits.js
@@ -1,6 +1,11 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import ProfitsTable from "main/components/Commons/ProfitsTable"
+import moment from "moment";
+
+function timestampToDate(timestamp) {
+    return moment(timestamp).format('YYYY-MM-DD');
+}
 
 /*
 const dummyData = [
@@ -13,13 +18,17 @@ const dummyData = [
 
 // add parameters 
 const Profits = ({userCommons, profits}) => {
+    const profitsForTable = profits && profits.map(profit => ({
+        date: timestampToDate(profit.timestamp),
+        ...profit
+    }));
     return (
         <Card>
             <Card.Header as="h5">Profits</Card.Header>
             <Card.Body>
                 {/* change 4am to admin-appointed time? And consider adding milk bottle as decoration */}
                 <Card.Title>You will earn profits from milking your cows everyday at 4am.</Card.Title>
-                <ProfitsTable profits={dummyData} />
+                <ProfitsTable profits={profitsForTable} />
             </Card.Body>
         </Card>
     );
