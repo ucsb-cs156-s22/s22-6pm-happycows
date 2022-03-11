a4b8356d82ca4fbf1bdde3857c309b828716ff68
rsdharmaji <59861195+rsdharmaji@users.noreply.github.com>
2022-03-10 22:26:48 -0800

    rd - fix missing import

--- src/test/java/edu/ucsb/cs156/happiercows/controllers/CommonsControllerTests.java
+++ src/test/java/edu/ucsb/cs156/happiercows/controllers/CommonsControllerTests.java
@@ -16,6 +16,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
