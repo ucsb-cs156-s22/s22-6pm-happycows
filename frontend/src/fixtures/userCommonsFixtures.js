0843ab9998b6674d4d8f4e1b4c72d4084251691b
zackmarks <72716695+zackmarks@users.noreply.github.com>
2022-05-28 19:07:41 -0700

    Update userCommonsFixtures.js
    zm - 6pm3 - added fixtures for 1, 3, 5, and 10 userCommons. for testing other parts of the site

--- frontend/src/fixtures/userCommonsFixtures.js
+++ frontend/src/fixtures/userCommonsFixtures.js
@@ -8,7 +8,7 @@ const userCommonsFixtures = {
             },
             "commons":  {
                 "id":1,
-                "name":"Anika's cows",
+                "name":"Table 11",
                 "day": 5,
                 "endDate":"6/11/2021",
                 "totalPlayers": 50,
@@ -17,6 +17,321 @@ const userCommonsFixtures = {
             "totalWealth" : 1000,
             "cowHealth": 98.0,
             "numOfCows": 5
+        }
+    ],
+    threeUserCommons: 
+    [
+        {
+            "id":1,
+            "user": {
+                "id": 1
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 1000,
+            "cowHealth": 93.0,
+            "numOfCows": 8
+        },
+        {
+            "id":2,
+            "user": {
+                "id": 2
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 1000,
+            "cowHealth": 98.0,
+            "numOfCows": 5
+        },
+        {
+            "id":3,
+            "user": {
+                "id": 3
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 100000,
+            "cowHealth": 2.0,
+            "numOfCows": 1000
+        }
+    ],
+    fiveUserCommons: 
+    [
+        {
+            "id":1,
+            "user": {
+                "id": 1
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 1000,
+            "cowHealth": 93.0,
+            "numOfCows": 8
+        },
+        {
+            "id":2,
+            "user": {
+                "id": 2
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 1000,
+            "cowHealth": 98.0,
+            "numOfCows": 5
+        },
+        {
+            "id":3,
+            "user": {
+                "id": 3
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 100000,
+            "cowHealth": 2.0,
+            "numOfCows": 1000
+        },
+        {
+            "id":4,
+            "user": {
+                "id": 3
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 50,
+            "cowHealth": 84.0,
+            "numOfCows": 100
+        },
+        {
+            "id":5,
+            "user": {
+                "id": 5
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 800,
+            "cowHealth": 72.0,
+            "numOfCows": 60
+        }
+    ],
+    tenUserCommons: 
+    [
+        {
+            "id":1,
+            "user": {
+                "id": 1
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 1000,
+            "cowHealth": 93.0,
+            "numOfCows": 8
+        },
+        {
+            "id":2,
+            "user": {
+                "id": 2
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 1000,
+            "cowHealth": 98.0,
+            "numOfCows": 5
+        },
+        {
+            "id":3,
+            "user": {
+                "id": 3
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 100000,
+            "cowHealth": 2.0,
+            "numOfCows": 1000
+        },
+        {
+            "id":4,
+            "user": {
+                "id": 3
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 50,
+            "cowHealth": 84.0,
+            "numOfCows": 100
+        },
+        {
+            "id":5,
+            "user": {
+                "id": 5
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 800,
+            "cowHealth": 72.0,
+            "numOfCows": 60
+        },
+        {
+            "id":6,
+            "user": {
+                "id": 6
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 1000,
+            "cowHealth": 93.0,
+            "numOfCows": 8
+        },
+        {
+            "id":7,
+            "user": {
+                "id": 7
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 1000,
+            "cowHealth": 98.0,
+            "numOfCows": 5
+        },
+        {
+            "id":8,
+            "user": {
+                "id": 8
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 100000,
+            "cowHealth": 2.0,
+            "numOfCows": 1000
+        },
+        {
+            "id":9,
+            "user": {
+                "id": 9
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 50,
+            "cowHealth": 84.0,
+            "numOfCows": 100
+        },
+        {
+            "id":10,
+            "user": {
+                "id": 10
+            },
+            "commons":  {
+                "id":1,
+                "name":"Table 11",
+                "day": 5,
+                "endDate":"6/11/2021",
+                "totalPlayers": 50,
+                "cowPrice": 15,
+            },
+            "totalWealth" : 800,
+            "cowHealth": 72.0,
+            "numOfCows": 60
         }
     ]
 }
