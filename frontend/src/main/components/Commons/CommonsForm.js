c8cb5da5bdd391951be1140b8e785f574e94f18c
Neel Murthy <neelmurthy1@gmail.com>
2022-05-31 14:31:54 -0700

    added degradationRate to CommonsForm and CommonsTable

--- frontend/src/main/components/Commons/CommonsForm.js
+++ frontend/src/main/components/Commons/CommonsForm.js
@@ -119,6 +119,27 @@ function CommonsForm({ initialCommons, submitAction, buttonLabel = "Create" }) {
           {errors.startingDate?.message}
         </Form.Control.Feedback>
       </Form.Group>
+
+      <Form.Group className="mb-3">
+        <Form.Label htmlFor="degradationRate">Degradation Rate</Form.Label>
+        <Form.Control
+          data-testid={`${testid}-degradationRate`}
+          id="degradationRate"
+          type="number"
+          step="0.01"
+          isInvalid={!!errors.degradationRate}
+          {...register("degradationRate", {
+            valueAsNumber: true,
+            required: "Degradation rate is required",
+            min: { value: 0.01, message: "Degradation rate must be positive" },
+            max: { value: 1.00, message: "Degradation rate must be at most 1" },
+          })}
+        />
+        <Form.Control.Feedback type="invalid">
+          {errors.degradationRate?.message}
+        </Form.Control.Feedback>
+      </Form.Group>
+
       <Button type="submit" data-testid="CommonsForm-Submit-Button">{ buttonLabel }</Button>
     </Form>
   );
