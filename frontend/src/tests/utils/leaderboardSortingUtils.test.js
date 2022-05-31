4b2b64805ead80961b2718f0c884e8daf0091f4b
D-Jerez <djerez@ucsb.edu>
2022-05-31 12:17:44 -0700

    dj - add sortByCowHealth tests

--- frontend/src/tests/utils/leaderboardSortingUtils.test.js
+++ frontend/src/tests/utils/leaderboardSortingUtils.test.js
@@ -9,6 +9,9 @@ describe("leaderboardSortingUtils tests", () => {
     tenUserCommons
   } = userCommonsFixtures;
 
+//-----------------------------//
+//        Wealth Tests
+//----------------------------//
   test("sortByWealth", () => {
     const sortedUserCommons = sortByWealth(fiveUserCommons);
     const expectedWealths = [100000, 1000, 1000, 800, 50];
@@ -39,6 +42,43 @@ describe("leaderboardSortingUtils tests", () => {
     const expectedWealths = [100000, 100000, 1000, 1000, 1000, 1000, 800, 800, 50, 50];
     for (i in expectedWealths){
       expect(sortedUserCommons[i].totalWealth).toBe(expectedWealths[i]);
+    }
+  });
+
+  //-----------------------------//
+  //      Cow Health Tests
+  //----------------------------//
+
+  test("sortByCowHealth", () => {
+    const sortedUserCommons = sortByCowHealth(fiveUserCommons);
+    const expectedCowHealths = [98.0, 93.0, 84.0, 72.0, 2.0];
+    for (i in expectedCowHealths){
+      expect(sortedUserCommons[i].cowHealth).toBe(expectedCowHealths[i]);
+    }
+  });
+
+  test("sortByCowHealthReturnOne", () => {
+    const sortedUserCommons = sortByCowHealth(tenUserCommons, 1);
+    expect(sortedUserCommons.length).toBe(1);
+    expect(sortedUserCommons[0].cowHealth).toBe(100000);
+  });
+
+  test("sortByCowHealthReturnThree", () => {
+    const sortedUserCommons = sortByCowHealth(tenUserCommons, 3);
+    expect(sortedUserCommons.length).toBe(3);
+    const expectedCowHealths = [100000, 100000, 1000];
+    for (i in expectedCowHealths){
+      expect(sortedUserCommons[i].cowHealth).toBe(expectedCowHealths[i]);
+    }
+  });
+
+  // Expected behavior here is to just return the full sorted array of userCommons
+  test("sortByCowHealthExpectTooMany", () => {
+    const sortedUserCommons = sortByCowHealth(tenUserCommons, 25);
+    expect(sortedUserCommons.length).toBe(tenUserCommons.length);
+    const expectedCowHealths = [100000, 100000, 1000, 1000, 1000, 1000, 800, 800, 50, 50];
+    for (i in expectedCowHealths){
+      expect(sortedUserCommons[i].cowHealth).toBe(expectedCowHealths[i]);
     }
   });
 
