68dd28fb5c3a8ad1d7ce8c206da9ab302635d026
D-Jerez <djerez@ucsb.edu>
2022-05-31 14:42:48 -0700

    dj - add sortByWealth, sortByNumCows, and sortByCowHealth functions

--- frontend/src/main/utils/leaderboardSortingUtils.js
+++ frontend/src/main/utils/leaderboardSortingUtils.js
@@ -1,16 +1,20 @@
 import userCommonsFixtures from "../../fixtures/userCommonsFixtures"
 
 export function sortByWealth(userCommonsArray, returnArraySize = userCommonsArray.length){
-  //stub
-  return [];
+  return userCommonsArray.sort((a, b) => {
+    return a.totalWealth - b.totalWealth;
+  }).slice(0, returnArraySize);
 }
 
 export function sortByNumCows(userCommonsArray, returnArraySize = userCommonsArray.length){
-  //stub
-  return [];
+  return userCommonsArray.sort((a, b) => {
+    return a.numOfCows - b.numOfCows;
+  }).slice(0, returnArraySize);
 }
 
 export function sortByCowHealth(userCommonsArray, returnArraySize = userCommonsArray.length){
-  //stub
-  return [];
+  //sorts in decreasing order, so the comparison function returns a negative when the first parameter is larger
+  return userCommonsArray.sort((a, b) => {
+    return b.cowHealth - a.cowHealth;
+  }).slice(0, returnArraySize);
 }
