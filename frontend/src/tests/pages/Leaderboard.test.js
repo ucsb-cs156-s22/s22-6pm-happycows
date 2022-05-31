0100a0120b3852743ddda8ef221f6cfbf4557a5d
Michelle Ly <myly@umail.ucsb.edu>
2022-05-31 10:54:12 -0700

    ml-added routing for placeholder page, need to create button + tests, unsure if placeholder page works

---
+++ frontend/src/tests/pages/Leaderboard.test.js
@@ -0,0 +1,4 @@
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { MemoryRouter } from "react-router-dom";
+import axios from "axios";
