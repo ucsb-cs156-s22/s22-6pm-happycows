626ba81cb327ebbfa253d5631c86a413120fcf49
Michelle Ly <myly@umail.ucsb.edu>
2022-06-01 23:26:52 -0700

    ml-testing commonsOverview to see if the tests passed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "main/pages/HomePage";
 import LoginPage from "main/pages/LoginPage";
 import ProfilePage from "main/pages/ProfilePage";
+import LeaderboardPage from "main/pages/LeaderboardPage";
 
 import AdminUsersPage from "main/pages/AdminUsersPage";
 import AdminCreateCommonsPage from "main/pages/AdminCreateCommonsPage";
@@ -24,6 +25,7 @@ function App() {
           !(hasRole(currentUser, "ROLE_ADMIN") || hasRole(currentUser, "ROLE_USER")) && <Route path="/" element={<LoginPage />} />
         }
         <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/leaderboard/:commonsId" element={<LeaderboardPage />}/>
         {
           hasRole(currentUser, "ROLE_ADMIN") && <Route path="/admin/users" element={<AdminUsersPage />} />
         }
--- frontend/src/main/components/Commons/CommonsOverview.js
+++ frontend/src/main/components/Commons/CommonsOverview.js
@@ -1,14 +1,24 @@
+
 import React from "react";
-import { Card } from "react-bootstrap";
+import { _Container, Row, Card, Col, Button, Router } from "react-bootstrap";
+import { Link, _useNavigate } from "react-router-dom";
 
 export default function CommonsOverview({ commons }) {
+    
     return (
-        <Card data-testid="CommonsOverview">
-            <Card.Header as="h5">Announcements</Card.Header>
-            <Card.Body>
-                <Card.Title>Today is day {commons.day}! This game will end on {commons.endDate}.</Card.Title>
-                <Card.Text>Total Players: {commons.totalPlayers}</Card.Text>
-            </Card.Body>
-        </Card>
+            <Card data-testid="CommonsOverview">
+                <Card.Header as="h5">Announcements</Card.Header>
+                <Card.Body>
+                    <Row>
+                        <Col>
+                        <Card.Title>Today is day {commons.day}! This game will end on {commons.endDate}.</Card.Title>
+                        <Card.Text>Total Players: {commons.totalPlayers}</Card.Text>
+                        </Col>
+                        <Col>
+                        {/* <Button variant="outline-success" as={Link} to={`/leaderboard/${commons.id}`} data-testid={"user-leaderboard-button"}>Leaderboard</Button> */}
+                        </Col>
+                    </Row>
+                </Card.Body>
+            </Card>
     );
 }; 
