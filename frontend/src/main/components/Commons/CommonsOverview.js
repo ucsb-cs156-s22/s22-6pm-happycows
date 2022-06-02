3459f2d1def16c8c258ca69b2703f98c4eb16d05
Michelle Ly <myly@umail.ucsb.edu>
2022-06-02 01:09:44 -0700

    ml-tests pass for leaderboard button for user

--- frontend/src/main/components/Commons/CommonsOverview.js
+++ frontend/src/main/components/Commons/CommonsOverview.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { _Container, Row, _Router, Card, Col, Button } from "react-bootstrap";
+import { _Link, _useNavigate } from "react-router-dom";
 
 export default function CommonsOverview({ commons }) {
     return (
         <Card data-testid="CommonsOverview">
             <Card.Header as="h5">Announcements</Card.Header>
             <Card.Body>
-                <Card.Title>Today is day {commons.day}! This game will end on {commons.endDate}.</Card.Title>
-                <Card.Text>Total Players: {commons.totalPlayers}</Card.Text>
+                <Row>
+                    <Col>
+                    <Card.Title>Today is day {commons.day}! This game will end on {commons.endDate}.</Card.Title>
+                    <Card.Text>Total Players: {commons.totalPlayers}</Card.Text>
+                    </Col>
+                    <Col>
+                    <Button variant="outline-success" href={`/leaderboard/${commons.id}`}>Leaderboard</Button>
+                    </Col>
+                </Row>
             </Card.Body>
         </Card>
     );
