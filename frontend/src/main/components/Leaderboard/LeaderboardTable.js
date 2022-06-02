07f7e1026f6d767a4424d5fad8bad1a3b5b97765
Stanley Su <stanleysu@ucsb.edu>
2022-06-02 02:08:58 -0700

    ss - adding LeaderboardTable to pr

---
+++ frontend/src/main/components/Leaderboard/LeaderboardTable.js
@@ -0,0 +1,46 @@
+import OurTable from "main/components/OurTable";
+import { hasRole } from "main/utils/currentUser";
+
+// should take in a players list from a commons
+export default function LeaderboardTable({ leaderboardUsers , currentUser }) {
+
+    const columns = [
+        {
+            Header: 'Full Name',
+            accessor: 'user.fullName', 
+        },
+        {
+            Header: 'Total Wealth',
+            accessor: 'totalWealth',
+        },
+        {
+            Header: 'Cows Owned',
+            accessor: 'numOfCows', 
+        },
+        {
+            Header: 'Cow Health',
+            accessor: 'cowHealth', 
+        }
+    ];
+
+    const testid = "LeaderboardTable";
+
+    /* Temp filler for admin leaderboard table */
+
+    const columnsIfAdmin = [
+        {
+            Header: '(Admin) User Id',
+            accessor: 'id'
+        },
+        ...columns
+
+    ];
+
+    const columnsToDisplay = hasRole(currentUser, "ROLE_ADMIN") ? columnsIfAdmin : columns;
+
+    return <OurTable
+        data={leaderboardUsers}
+        columns={columnsToDisplay}
+        testid={testid}
+    />;
+};
