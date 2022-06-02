a9f345a647402fed72c6f0f961f624d4161c4ee8
Michelle Ly <myly@umail.ucsb.edu>
2022-06-02 11:37:17 -0700

    ml-adding test for user leaderboard button, doesn't work yet

--- frontend/src/tests/components/Commons/CommonsOverview.test.js
+++ frontend/src/tests/components/Commons/CommonsOverview.test.js
@@ -7,8 +7,16 @@ import AxiosMockAdapter from "axios-mock-adapter";
 import CommonsOverview from "main/components/Commons/CommonsOverview"; 
 import PlayPage from "main/pages/PlayPage";
 import commonsFixtures from "fixtures/commonsFixtures"; 
+import leaderboardFixtures from "fixtures/leaderboardFixtures";
 import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
 import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
+
+jest.mock("react-router-dom", () => ({
+    ...jest.requireActual("react-router-dom"),
+    useParams: () => ({
+        commonsId: 1
+    })
+}));
 
 describe("CommonsOverview tests", () => {
 
@@ -30,6 +38,8 @@ describe("CommonsOverview tests", () => {
     test("Redirects to the LeaderboardPage when you click visit", async () => {
         apiCurrentUserFixtures.userOnly.user.commons = commonsFixtures.oneCommons;
         axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
+        axiosMock.onGet("/api/commons", {params: {commons_id:1}}).reply(200, commonsFixtures.oneCommons);
+        axiosMock.onGet("/api/leaderboard/all").reply(200, leaderboardFixtures.threeUserCommonsLB);
         render(
             <QueryClientProvider client={queryClient}>
                 <MemoryRouter>
@@ -37,8 +47,8 @@ describe("CommonsOverview tests", () => {
                 </MemoryRouter>
             </QueryClientProvider>
         );
-        expect(await screen.findByTestId("commonsCard-button-Leaderboard-1")).toBeInTheDocument();
-        const joinButton = screen.getByTestId("commonsCard-button-Leaderboard-1");
+        expect(await screen.findByTestId("user-leaderboard-button")).toBeInTheDocument();
+        const joinButton = screen.getByTestId("user-leaderboard-button");
         fireEvent.click(joinButton);
     });
 });
