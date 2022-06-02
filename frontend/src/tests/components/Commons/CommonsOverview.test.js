15256d79335930fc33d9566cfe52e2ddfc0c3abd
Michelle Ly <myly@umail.ucsb.edu>
2022-06-02 07:59:13 -0700

    ml-testing deployment

--- frontend/src/tests/components/Commons/CommonsOverview.test.js
+++ frontend/src/tests/components/Commons/CommonsOverview.test.js
@@ -1,8 +1,25 @@
-import { render } from "@testing-library/react";
+import { _fireEvent, render, _screen } from "@testing-library/react";
+import { _QueryClient, _QueryClientProvider } from "react-query";
+import { _MemoryRouter } from "react-router-dom";
+// import axios from "axios";
+// import AxiosMockAdapter from "axios-mock-adapter";
+
 import CommonsOverview from "main/components/Commons/CommonsOverview"; 
+// import PlayPage from "main/pages/PlayPage";
 import commonsFixtures from "fixtures/commonsFixtures"; 
+// import { apiCurrentUserFixtures } from "fixtures/currentUserFixtures";
+// import { systemInfoFixtures } from "fixtures/systemInfoFixtures";
 
 describe("CommonsOverview tests", () => {
+
+    // const queryClient = new QueryClient();
+    // const axiosMock = new AxiosMockAdapter(axios);
+
+    // beforeEach(() => {
+    //     axiosMock.reset();
+    //     axiosMock.resetHistory();
+    //     axiosMock.onGet("/api/systemInfo").reply(200, systemInfoFixtures.showingNeither);
+    // });
 
     test("renders without crashing", () => {
         render(
