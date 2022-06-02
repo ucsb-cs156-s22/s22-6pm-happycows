afb28cfe4d88b4e0b03929b5d118db12556ca871
Michelle Ly <myly@umail.ucsb.edu>
2022-06-01 18:10:46 -0700

    ml - testing to see if button works and uses correct route

--- frontend/src/main/components/Commons/CommonsOverview.js
+++ frontend/src/main/components/Commons/CommonsOverview.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Col } from "react-bootstrap";
 
 export default function CommonsOverview({ commons }) {
+    
+    let navigate = useNavigate();
+    const leaderboardButtonClick = (id) => { navigate("/leaderboard/" + id) };
+    
     return (
         <Card data-testid="CommonsOverview">
             <Card.Header as="h5">Announcements</Card.Header>
             <Card.Body>
-                <Card.Title>Today is day {commons.day}! This game will end on {commons.endDate}.</Card.Title>
-                <Card.Text>Total Players: {commons.totalPlayers}</Card.Text>
+                <Row>
+                    <Col>
+                    <Card.Title>Today is day {commons.day}! This game will end on {commons.endDate}.</Card.Title>
+                    <Card.Text>Total Players: {commons.totalPlayers}</Card.Text>
+                    </Col>
+                    <Col>
+                    <Button variant="outline-success" buttonLink={leaderboardButtonClick} data-testid={"user-leaderboard-button"}>Leaderboard</Button>
+                    </Col>
+                </Row>
             </Card.Body>
         </Card>
     );
