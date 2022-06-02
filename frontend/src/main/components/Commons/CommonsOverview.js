e7bf9b6841a4d19bf7f4797a12ef6a44195d7606
Michelle Ly <myly@umail.ucsb.edu>
2022-06-02 09:34:16 -0700

    ml-merged branches to get access to lb fixtures

--- frontend/src/main/components/Commons/CommonsOverview.js
+++ frontend/src/main/components/Commons/CommonsOverview.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { _Container, _Row, Card, Col, Button } from "react-bootstrap";
-import { Link, _useNavigate } from "react-router-dom";
+import { _Container, Row, _Router, Card, Col, Button } from "react-bootstrap";
+import { _Link, _useNavigate } from "react-router-dom";
 
 export default function CommonsOverview({ commons }) {
     return (
         <Card data-testid="CommonsOverview">
             <Card.Header as="h5">Announcements</Card.Header>
             <Card.Body>
-                <Card.Title>Today is day {commons.day}! This game will end on {commons.endDate}.</Card.Title>
-                <Card.Text>Total Players: {commons.totalPlayers}</Card.Text>
-                <Col>
-                    <Button variant="outline-success" as={Link} to={`/leaderboard/${commons.id}`} data-testid={"user-leaderboard-button"}>Leaderboard</Button>
-                </Col>
+                <Row>
+                    <Col>
+                    <Card.Title>Today is day {commons.day}! This game will end on {commons.endDate}.</Card.Title>
+                    <Card.Text>Total Players: {commons.totalPlayers}</Card.Text>
+                    </Col>
+                    <Col>
+                    <Button variant="outline-success" href={`/leaderboard/${commons.id}`}>Leaderboard</Button>
+                    </Col>
+                </Row>
             </Card.Body>
         </Card>
     );
