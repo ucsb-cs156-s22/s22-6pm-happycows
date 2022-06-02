0e1064b7eb38ac81cfad4318d540f121e4b3dd56
Michelle Ly <myly@umail.ucsb.edu>
2022-06-02 10:11:42 -0700

    ml-brought code back to how it originally was without leaderboard button, changes to app.js, commonsoverview.js, package-lock.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "main/pages/HomePage";
 import LoginPage from "main/pages/LoginPage";
 import ProfilePage from "main/pages/ProfilePage";
-import LeaderboardPage from "main/pages/LeaderboardPage";
 
 import AdminUsersPage from "main/pages/AdminUsersPage";
 import AdminCreateCommonsPage from "main/pages/AdminCreateCommonsPage";
@@ -25,7 +25,6 @@ function App() {
           !(hasRole(currentUser, "ROLE_ADMIN") || hasRole(currentUser, "ROLE_USER")) && <Route path="/" element={<LoginPage />} />
         }
         <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/leaderboard/:commonsId" element={<LeaderboardPage />}/>
         {
           hasRole(currentUser, "ROLE_ADMIN") && <Route path="/admin/users" element={<AdminUsersPage />} />
         }
