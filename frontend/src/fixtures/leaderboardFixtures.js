5fc5d784e94b25c07a5230d2021fab36eed45d1a
Stanley Su <stanleysu@ucsb.edu>
2022-06-02 02:26:25 -0700

    ss - adding leaderboardFixtures to pr

---
+++ frontend/src/fixtures/leaderboardFixtures.js
@@ -0,0 +1,162 @@
+const leaderboardFixtures = {
+    oneUserCommonsLB: 
+    [
+        {
+            "id":1,
+            "userId": 1,
+            "commonsId": 1,
+            "totalWealth" : 1000,
+            "numOfCows": 5
+        }
+    ],
+    threeUserCommonsLB:
+    [
+        {
+            "id":1,
+            "userId": 1,
+            "commonsId": 1,
+            "totalWealth" : 1000,
+            "numOfCows": 8
+        },
+        {
+            "id":2,
+            "userId": 2,
+            "commonsId": 1,
+            "totalWealth" : 1000,
+            "numOfCows": 5
+        },
+        {
+            "id":3,
+            "userId": 3,
+            "commonsId": 1,
+            "totalWealth" : 100000,
+            "numOfCows": 1000
+        }
+    ],
+    fiveUserCommonsLB: 
+    [
+        {
+            "id":1,
+            "userId": 1,
+            "commonsId": 1,
+            "totalWealth" : 1000,
+            "cowHealth": 93.0,
+            "numOfCows": 8
+        },
+        {
+            "id":2,
+            "userId": 2,
+            "commonsId": 1,
+            "totalWealth" : 1000,
+            "cowHealth": 98.0,
+            "numOfCows": 5
+        },
+        {
+            "id":3,
+            "userId": 3,
+            "commonsId": 1,
+            "totalWealth" : 100000,
+            "cowHealth": 2.0,
+            "numOfCows": 1000
+        },
+        {
+            "id":4,
+            "userId": 4,
+            "commonsId": 1,
+            "totalWealth" : 50,
+            "cowHealth": 84.0,
+            "numOfCows": 100
+        },
+        {
+            "id":5,
+            "userId": 5,
+            "commonsId": 1,
+            "totalWealth" : 800,
+            "numOfCows": 60
+        }
+    ],
+    tenUserCommonsLB: 
+    [
+        {
+            "id":1,
+            "userId": 1,
+            "commonsId": 1,
+            "totalWealth" : 1000,
+            "cowHealth": 93.0,
+            "numOfCows": 8
+        },
+        {
+            "id":2,
+            "userId": 2,
+            "commonsId": 1,
+            "totalWealth" : 1000,
+            "cowHealth": 98.0,
+            "numOfCows": 5
+        },
+        {
+            "id":3,
+            "userId": 3,
+            "commonsId": 1,
+            "totalWealth" : 100000,
+            "cowHealth": 2.0,
+            "numOfCows": 1000
+        },
+        {
+            "id":4,
+            "userId": 4,
+            "commonsId": 1,
+            "totalWealth" : 50,
+            "cowHealth": 84.0,
+            "numOfCows": 100
+        },
+        {
+            "id":5,
+            "userId": 5,
+            "commonsId": 1,
+            "totalWealth" : 800,
+            "cowHealth": 72.0,
+            "numOfCows": 60
+        },
+        {
+            "id":6,
+            "userId": 6,
+            "commonsId": 1,
+            "totalWealth" : 1000,
+            "cowHealth": 93.0,
+            "numOfCows": 8
+        },
+        {
+            "id":7,
+            "userId": 7,
+            "commonsId": 1,
+            "totalWealth" : 1000,
+            "cowHealth": 98.0,
+            "numOfCows": 5
+        },
+        {
+            "id":8,
+            "userId": 8,
+            "commonsId":  1,
+            "totalWealth" : 100000,
+            "cowHealth": 2.0,
+            "numOfCows": 1000
+        },
+        {
+            "id":9,
+            "userId": 9,
+            "commonsId":  1,
+            "totalWealth" : 50,
+            "cowHealth": 84.0,
+            "numOfCows": 100
+        },
+        {
+            "id":10,
+            "userId": 10,
+            "commonsId": 1,
+            "totalWealth" : 800,
+            "numOfCows": 60
+        }
+    ]
+}
+
+export default leaderboardFixtures; 
