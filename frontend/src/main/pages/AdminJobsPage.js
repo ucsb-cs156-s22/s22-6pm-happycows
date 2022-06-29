88947a58152d5c1004dc5276af28db0c29e9cf0d
Phillip Conrad <pconrad@cs.ucsb.edu>
2022-06-29 13:18:08 -0700

    pc - add placeholder for Admin / Manage Jobs

---
+++ frontend/src/main/pages/AdminJobsPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+const AdminJobsPage = () => {
+
+    return (
+        <BasicLayout>
+            <h2>Manage Jobs (Admins)</h2>
+            <p>Coming Soon; for now, you may use the Swagger endpoints to manage jobs.</p>
+        </BasicLayout>
+    );
+};
+
+export default AdminJobsPage;
